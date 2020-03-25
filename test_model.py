abab09e973fb29df7304abc9cfd1fcf9a6410757
cheng-yuhan <60492481+cheng-yuhan@users.noreply.github.com>
2020-03-25 16:37:49 -0400

    Add files via upload

---
+++ test_model.py
@@ -0,0 +1,17 @@
+from keras.models import load_model
+load_model = load_model("choking_model.h5")
+from test_loader import test_loader
+import matplotlib.pyplot as plt
+
+test_choking_x  = test_loader.read("test_choking")
+test_choking_x = test_choking_x.astype("float32") / 255
+
+y_pred = load_model.predict(test_choking_x)
+
+time = range(1, len(y_pred) + 1)
+plt.plot(time, y_pred)
+plt.xlabel("time")
+plt.ylabel("probability")
+plt.title('choking prediction')
+plt.legend()
+plt.show()
